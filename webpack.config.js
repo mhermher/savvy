b9cf04644aa26d33efc648c910013cec65291332
Mher Alaverdyan <mhermher@live.com>
2020-10-08 00:17:25 -0700

    output dts

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
         ]
     },
     output : {
-        filename : 'savvy.js',
+        filename : 'index.js',
         path : path.resolve(__dirname, 'dist'),
         library : 'savvy',
         libraryTarget : 'umd',
